c0a506510dcccfe0bf2562e98ac3732a29029296
Brian Brown <brbrowngeo@gmail.com>
2019-04-09 07:40:23 -0400

    material packages

--- src/core/Header.tsx
+++ src/core/Header.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
 import './Header.scss';
-class Header extends Component {
-  render() {
+
+function Header() {
     return (
         <header className="App-header">
             <strong>Reminders</strong>
         </header>
     );
-  }
 }
 
 export default Header;
