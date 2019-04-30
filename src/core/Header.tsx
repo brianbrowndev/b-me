dd49f381bef4b2c4150f4d4aaf32b4a50eda3c4c
Brian Brown <brbrowngeo@gmail.com>
2019-04-30 07:55:55 -0400

    simplify styling, basic login form

--- src/core/Header.tsx
+++ src/core/Header.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import { Link } from "react-router-dom";
+
 import './Header.scss';
 
 function Header() {
     return (
         <header className="App-header">
-            <strong>Reminders</strong>
+            <Link to="/"><strong className="App-title">Reminders</strong></Link>
+            <Link to="/login">login</Link>
         </header>
-    );
+   );
 }
 
 export default Header;
---
+++ src/core/Login.tsx
@@ -0,0 +1,50 @@
+import React, { Component, Fragment, useState } from 'react';
+
+import './Login.scss';
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [pw, setPw] = useState("");
+
+    function login(event: React.FormEvent) {
+        event.preventDefault();
+        console.log(username)
+        console.log(pw)
+    }
+
+    return (
+        <Fragment>
+            <div>
+                <strong>Login</strong>
+            </div>
+            <form onSubmit={login}>
+                <div className="Login-input">
+                    <label>
+                        username: 
+                        <input 
+                            type="text" 
+                            onChange={e => setUsername(e.target.value)}
+                            required
+                            name="username">
+                        </input>
+                    </label>
+                </div>
+                <div className="Login-input">
+                    <label>
+                        password: 
+                        <input 
+                            type="password" 
+                            onChange={e => setPw(e.target.value)}
+                            required
+                            name="pw">
+                        </input>
+                    </label>
+                </div>
+                <input type="submit" value="Submit" />
+
+            </form>
+        </Fragment>
+   );
+}
+
+export default Login;
