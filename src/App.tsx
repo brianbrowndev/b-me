2725cc61056e4763fbc65f06727ee74a0df51f22
Brian Brown <brbrowngeo@gmail.com>
2019-04-30 07:55:55 -0400

    simplify styling, basic login form

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Header from './core/Header';
+import Login from './core/Login';
 
 import PublicReminderList from './reminders/PublicReminderList';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <main>
-          <PublicReminderList />
-
-        </main>
-        <footer></footer>
-      </div>
+      <Router>
+          <div className="App">
+            <Header />
+            <main>
+              <Route exact path="/" component={PublicReminderList} />
+              <Route exact path="/login" component={Login} />
+            </main>
+            <footer></footer>
+          </div>
+      </Router>
     );
   }
 }
