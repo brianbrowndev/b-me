065012e4ee94fbe9d4eb7f763c33f97c870761fb
Brian Brown <brbrowngeo@gmail.com>
2019-07-19 07:39:30 -0400

    refactor event crud to add/view, instead of list

--- src/events/lists/EventListItemAdd.tsx
+++ src/events/lists/EventListItemAdd.tsx
@@ -1,12 +1,12 @@
-import React, {  useEffect, Fragment, useState, useRef,  RefObject } from 'react';
-import { Event } from '../../common/client/index';
+import React, {  Fragment, useState, useRef,  RefObject } from 'react';
+import { Event, SwaggerException } from '../../common/client/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './EventListItemAdd.scss';
+import EventApi  from '../../common/client/EventApi';
 
 interface EventListItemAddProps {
-    onChange (evt:React.FormEvent):void;
-    event: Event;
-    onSubmit ():void;
+    onAdd(event: Event):void;
+    onError(message:string):void;
 }
 
 function EventListItemAdd(props:EventListItemAddProps) {
@@ -15,35 +15,53 @@ function EventListItemAdd(props:EventListItemAddProps) {
 
     const [isSaving, setIsSaving] = useState<Boolean>();
 
+    // add
+    const initialAddEventState = {} as Event;
+    const [addEvent, setAddEvent] = useState<Event>({...initialAddEventState});
+
+    const onNameChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = evt.target as any
+        setAddEvent({ ...addEvent, [name]: value })
+    }
+
+    const propertyOf = (e: keyof Event) => e;
+
+    const handleEventAdd = () => { 
+        setIsSaving(true);
+        EventApi.createEvent(addEvent).then(result => {
+            props.onAdd(result);
+            // on success, reset to initial state
+            reset();
+        }).catch((err: SwaggerException) => {
+            props.onError(`Error adding event: ${err.message}`);
+            // on error, keep the current state but remove the pending
+            setIsSaving(false);
+        });
+    }
+
+    const reset = () => {
+        setIsSaving(false);
+        setAddEvent({...initialAddEventState});
+        if (inputEl !== null && inputEl.current != null) { 
+            inputEl.current.value = '';
+            inputEl.current.focus();
+        }
+    }
+
     const onFormSubmit = (evt: React.FormEvent) => {
         if (evt) evt.preventDefault();
         onSubmit();
     }
     const onSubmit = () => {
         if (validateModel()) {
-            setIsSaving(true);
-            props.onSubmit();
+            handleEventAdd();
         }
     }
-
     const validateModel = ():boolean => {
-        if (props.event.name == null || props.event.name == "") return false;
+        if (addEvent.name == null || addEvent.name == "") return false;
         return true;
     }
 
-    /**
-     * After an event is added, reset the form
-     */
-    useEffect(() => {
-        if (isSaving === true) {
-            setIsSaving(false);
-
-            if (inputEl !== null && inputEl.current != null) { 
-                inputEl.current.value = '';
-                inputEl.current.focus();
-            }
-        }
-    }, [props.event])
 
     return (
 
@@ -59,12 +77,12 @@ function EventListItemAdd(props:EventListItemAddProps) {
                             ref={inputEl} 
                             className="Event-add"
                             type="text" 
-                            onChange={props.onChange}
+                            onChange={onNameChange}
                             onBlur={onSubmit}
                             required
                             placeholder="Add an event"
                             disabled={isSaving ? true : false}
-                            name="name">
+                            name={propertyOf('name')}>
                         </input>
                     </div>
                 </form>
