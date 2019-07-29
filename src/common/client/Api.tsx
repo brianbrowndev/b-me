b92f8604fb165b7302f5bb9d500fc98744b3fc50
Brian Brown <bbrown@geodecisions.com>
2019-07-29 16:48:05 -0400

    org init

--- src/common/client/Api.tsx
+++ src/common/client/Api.tsx
@@ -1,8 +1,39 @@
+import { SwaggerException } from ".";
+
 /**
- * DEPRECATED - Use API derived from Nswag in /src/ommon/client
+ * IMPORTANT - This was deprecrated but having issues with NSwag not handling text
+ * Until text can be properly handled in generated client, will need to use this definition
+ * for retriving text org files
+ * 
  */
 
+
+function throwException(message: string, status: number, response: string, headers: { [key: string]: any; }, result?: any): any {
+    if(result !== null && result !== undefined)
+        throw result;
+    else
+        throw new SwaggerException(message, status, response, headers, null);
+}
+
 export const Api = {
+    getText(endpoint:string): Promise<string> {
+        let config = {headers:{}}
+
+        let token = localStorage.getItem('access_token') || null
+        config.headers = { 'Authorization': `Bearer ${token}` }
+        return fetch(`${process.env.REACT_APP_API}/${endpoint}`, config)
+            .then(r => {
+        const status = r.status;
+        let _headers: any = {}; if (r.headers && r.headers.forEach) { r.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return r.text().then((_responseText) => _responseText);
+        } else if (status === 400) {
+            return r.text().then((_responseText) => throwException("A server error occurred.", status, _responseText, _headers));
+        } else {
+            return r.text().then((_responseText) =>  throwException("A server error occurred.", status, _responseText, _headers));
+        }
+        })
+    },
     get<T>(endpoint:string): Promise<T> {
         let config = {headers:{}}
 
