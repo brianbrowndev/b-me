2236df0bad1bcea1ef5d5310b1285564b75c5aff
Brian Brown <brbrowngeo@gmail.com>
2019-08-02 14:00:04 -0400

    org content component

---
+++ src/org/OrgContent.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState, useMemo } from 'react';
+import OrgApi from '../common/client/OrgApi'
+import './OrgContent.scss';
+const DOMPurify = require('dompurify')
+
+
+// ideally the api would drive what content is available for org related items
+type OrgContentName  = 'raleigh';
+interface OrgContentProps {
+    name: OrgContentName
+}
+
+ 
+
+function OrgContent(props:OrgContentProps) {
+    const [text, setText] = useState('');
+    useEffect(
+        (() => {
+            switch (props.name) {
+                case 'raleigh':
+                    OrgApi.getPlaces('raleigh').then(t => setText(t))
+                    break;
+                default:
+                    break;
+            }
+            }), 
+        [] // only call the fetch once by passing in empty params
+    );
+
+    return useMemo(() => (
+        <div className="Org-content">
+            <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(text)}} />
+        </div>
+    ), [text]);
+}
+
+export default OrgContent;
