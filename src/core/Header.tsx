64da23bf36f4e3a1aec05032be09ac71f58c2569
Brian Brown <brbrowngeo@gmail.com>
2019-08-22 20:35:06 -0400

    collapse menu

--- src/core/Header.tsx
+++ src/core/Header.tsx
@@ -4,13 +4,12 @@ import React, { useContext, useState, Fragment } from 'react';
 import {AuthContext } from './Auth'
 
 import {
-  NavLink,
   withRouter,
   RouteComponentProps
 } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Typography, Divider,  createStyles, makeStyles, IconButton, Theme, Drawer, ListItem, List, ListItemText, ListSubheader, ListItemIcon, useScrollTrigger } from '@material-ui/core';
+import { Typography, Divider,  createStyles, makeStyles, IconButton, Theme, Drawer, ListItem, List, ListItemText, ListItemIcon, useScrollTrigger, Hidden, useTheme } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
@@ -26,6 +25,10 @@ const useStyles = makeStyles((theme: Theme) =>
       title: {
         flexGrow: 1
       },
+      logo: {
+        color:theme.palette.text.primary,
+        fontWeight:900
+      },
       appBar: {
         marginLeft: drawerWidth,
         [theme.breakpoints.up('sm')]: {
@@ -37,15 +40,22 @@ const useStyles = makeStyles((theme: Theme) =>
           width: drawerWidth,
           flexShrink: 0,
         },
+        backgroundColor: theme.palette.primary.light
       },
       toolbar: theme.mixins.toolbar,
       drawerPaper: {
         width: drawerWidth,
-        overflow:'hidden'
+        overflow:'hidden',
+        backgroundColor: theme.palette.primary.light,
+        boxShadow:'none'
       },
-      logo: {
-        color:'initial'
+      menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+          display: 'none',
+        },
       },
+
       login: {
         position:'relative',
         bottom:0,
@@ -53,7 +63,14 @@ const useStyles = makeStyles((theme: Theme) =>
       contentList: {
         flex:1,
         overflowY:'auto'
+      },
+      listTitle: {
+        fontWeight:theme.typography.fontWeightBold,
+      },
+      listIcon: {
+        color:'inherit'
       }
+
     })
   },
 );
@@ -80,12 +97,12 @@ function Header({ history }: RouteComponentProps) {
   const orgContext = useContext(OrgContext);
 
   const classes = useStyles();
+  const theme = useTheme();
 
   const logout = () => authContext.logout(() => history.push("/"));
 
   const [open, setOpen] = useState(false);
   const handleDrawerToggle = () => setOpen(!open);
-  const handleDrawerOpen = () => setOpen(true);
   const handleDrawerClose = () => setOpen(false);
 
 
@@ -95,20 +112,17 @@ function Header({ history }: RouteComponentProps) {
       <List
         component="div"
         className={classes.contentList}>
-{/* 
-      //   subheader={
-      //     <ListSubheader>
-      //       Org
-      //     </ListSubheader>
-      // }>
- */}
+        <AppLink to="/" exact={true} onClick={handleDrawerClose}>
+          <ListItem button>
+              <ListItemText primary="Home" classes={{primary: classes.listTitle}}/>
+          </ListItem>
+        </AppLink>
         {orgContext.routes().map(groupItem => 
-          <Fragment>
+          <Fragment key={groupItem.title}>
             <ListItem>
-              <ListItemText primary={groupItem.title} />
+              <ListItemText primary={groupItem.title} classes={{primary: classes.listTitle}}/>
             </ListItem>       
             <OrgGroupRouteList orgGroup={groupItem} onClick={handleDrawerClose} key={groupItem.title} nested={true}></OrgGroupRouteList>
-            {/* <Divider /> */}
           </Fragment>
         )}
       </List>
@@ -124,7 +138,7 @@ function Header({ history }: RouteComponentProps) {
           ) : (
             <AppLink to="/login" onClick={handleDrawerClose}>
               <ListItem button>
-                  <ListItemIcon><AccountCircleIcon /></ListItemIcon>
+                  <ListItemIcon className={classes.listIcon}><AccountCircleIcon className={classes.listIcon}/></ListItemIcon>
                   <ListItemText primary="Login" />
               </ListItem>
             </AppLink>
@@ -138,41 +152,53 @@ function Header({ history }: RouteComponentProps) {
       <ElevationScroll>
         <AppBar color="primary" elevation={1} className={classes.appBar}>
           <Toolbar>
-            <Typography variant="h6" className={classes.title} >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h4" className={classes.title} >
               <AppLink to="/">
                 <span className={classes.logo}>Me</span>
               </AppLink>
             </Typography>
-            <IconButton color="inherit" onClick={handleDrawerOpen}>
-              <MenuIcon />
-            </IconButton>
           </Toolbar>
 
         </AppBar>
       </ElevationScroll>
       <nav className={classes.drawer}>
+          <Hidden smUp>
+            <Drawer
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={open}
+              onClose={handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown>
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
 
-          {/* <Drawer
-            variant="temporary"
-            anchor="left"
-            open={open}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{keepMounted: true }} // Better open performance on mobile.
-          >
-            {drawer}
-          </Drawer> */}
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
 
       </nav>
     </Fragment>
--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -2,7 +2,8 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#fafafa",
+      main: "#ffffff",
+      light: "#fafafa",
     },
     secondary: {
       main: '#6B38FB',
